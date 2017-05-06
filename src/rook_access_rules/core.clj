0b4fd12f260a72bff4339d61fc35c740f9ed228b
David Banks <amoebae@gmail.com>
2017-05-06 13:08:53 +0100

    error policy support for middleware

--- src/rook_access_rules/core.clj
+++ src/rook_access_rules/core.clj
@@ -1,13 +1,9 @@
 (ns rook-access-rules.core
   (:require [buddy.auth.accessrules :as accessrules]))
 
-
-(defn wrap-with-access-rules [endpoint-handler metadata]
-  (when-let [rule-handler (:access-rule metadata)]
-    (accessrules/restrict endpoint-handler
-                          {:handler rule-handler})))
-
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn make-access-rule-middleware [error-policy]
+  (fn [endpoint-handler metadata]
+    (when-let [rule-handler (:access-rule metadata)]
+      (accessrules/restrict endpoint-handler
+                            {:handler rule-handler
+                             :on-error error-policy}))))
--- test/rook_access_rules/core_test.clj
+++ test/rook_access_rules/core_test.clj
@@ -3,10 +3,15 @@
             [rook-access-rules.core :as core]
             [buddy.auth.accessrules :as accessrules]
             [io.aviso.rook :as rook]
+            [ring.util.response :as r]
             [ring.mock.request :as mock]))
+
+(defn error-policy [request rejection-value]
+  (-> nil (r/response) (r/status 401)))
 
 (defn make-handler []
   (rook/namespace-handler
+   {:default-middleware (core/make-access-rule-middleware error-policy)}
    ['rook-access-rules.target-test-ns]))
 
 (deftest unrestricted-works
